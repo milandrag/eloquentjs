5f0924c6fee7707f7618896dd8ef58c71d60666e
Milan Dragovic <millandragovic@gmail.com>
2020-10-26 10:09:32 +0100

    Project Robot

---
+++ chapter-6/projectRobot.js
@@ -0,0 +1,228 @@
+var roads = [
+    "Alice's House-Bob's House",   "Alice's House-Cabin",
+    "Alice's House-Post Office",   "Bob's House-Town Hall",
+    "Daria's House-Ernie's House", "Daria's House-Town Hall",
+    "Ernie's House-Grete's House", "Grete's House-Farm",
+    "Grete's House-Shop",          "Marketplace-Farm",
+    "Marketplace-Post Office",     "Marketplace-Shop",
+    "Marketplace-Town Hall",       "Shop-Town Hall"
+  ];
+  
+
+  //SEPARATING STRING "ROUTES" TO -FROM- AND -TO- VARIABLES TO MAKE EACH OF ROUTES AS A VARIABLE
+  function buildGraph(edges) {
+    let graph = Object.create(null);
+    function addEdge(from, to) {
+      if (graph[from] == null) {
+        graph[from] = [to];
+      } else {
+        graph[from].push(to);
+      }
+    }
+    for (let [from, to] of edges.map(r => r.split("-"))) {
+      addEdge(from, to);
+      addEdge(to, from);
+    }
+    return graph;
+  }
+  
+  var roadGraph = buildGraph(roads);
+  
+  var VillageState = class VillageState {
+    constructor(place, parcels) {
+      this.place = place;
+      this.parcels = parcels;
+    }
+  
+    //CHECKING IF THERE ISE EXISTING ROUTE FOR THE NEXT DELIVERY
+    move(destination) {
+      if (!roadGraph[this.place].includes(destination)) {
+        return this;
+      } else {
+        let parcels = this.parcels.map(p => {
+          if (p.place != this.place) return p;
+          return {place: destination, address: p.address};
+        }).filter(p => p.place != p.address);
+        return new VillageState(destination, parcels);
+      }
+    }
+  }
+  //MEMORY => COLLECTING THE ROUTES WHERE DELIVERY HAS DONE SUCCESSFULLY
+  //
+  function runRobot(state, robot, memory) {
+    for (let turn = 0; turn++) {
+      if (state.parcels.length == 0) {
+        console.log(`Done in ${turn} turns`);
+        break;
+      }
+      let action = robot(state, memory);
+      state = state.move(action.direction);
+      memory = action.memory;
+      console.log(`Moved to ${action.direction}`);
+    }
+  }
+  // RANDOM PICKING PLACES WHERE THE ROBOT HAS NOT MADE A DELIVERY 
+  function randomPick(array) {
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+  }
+  
+  function randomRobot(state) {
+    return {direction: randomPick(roadGraph[state.place])};
+  }
+  
+  VillageState.random = function(parcelCount = 5) {
+    let parcels = [];
+    for (let i = 0; i < parcelCount; i++) {
+      let address = randomPick(Object.keys(roadGraph));
+      let place;
+      do {
+        place = randomPick(Object.keys(roadGraph));
+      } while (place == address);
+      parcels.push({place, address});
+    }
+    return new VillageState("Post Office", parcels);
+  };
+  
+  var mailRoute = [
+    "Alice's House", "Cabin", "Alice's House", "Bob's House",
+    "Town Hall", "Daria's House", "Ernie's House",
+    "Grete's House", "Shop", "Grete's House", "Farm",
+    "Marketplace", "Post Office"
+  ];
+  
+  function routeRobot(state, memory) {
+    if (memory.length == 0) {
+      memory = mailRoute;
+    }
+    return {direction: memory[0], memory: memory.slice(1)};
+  }
+
+  //FINDING THE SHORTEST ROUTES FROME ONE PLACE TO ANOTHER 
+  
+  function findRoute(graph, from, to) {
+    let work = [{at: from, route: []}];
+    for (let i = 0; i < work.length; i++) {
+      let {at, route} = work[i];
+      for (let place of graph[at]) {
+        if (place == to) return route.concat(place);
+        if (!work.some(w => w.at == place)) {
+          work.push({at: place, route: route.concat(place)});
+        }
+      }
+    }
+  }
+
+  //w
+  
+  function goalOrientedRobot({place, parcels}, route) {
+    if (route.length == 0) {
+      let parcel = parcels[0];
+      if (parcel.place != place) {
+        route = findRoute(roadGraph, place, parcel.place);
+      } else {
+        route = findRoute(roadGraph, place, parcel.address);
+      }
+    }
+    return {direction: route[0], memory: route.slice(1)};
+  }
+
+
+
+
+  /////// THE 1ST TASK
+  
+  function countSteps(state, robot, memory) {
+    for (let steps = 0;; steps++) {
+      if (state.parcels.length == 0) return steps;
+      let action = robot(state, memory);
+      state = state.move(action.direction);
+      memory = action.memory;
+    }
+  }
+  
+  function compareRobots(robot1, memory1, robot2, memory2) {
+    let total1 = 0, total2 = 0;
+    for (let i = 0; i < 100; i++) {
+      let state = VillageState.random();
+      total1 += countSteps(state, robot1, memory1);
+      total2 += countSteps(state, robot2, memory2);
+    }
+    console.log(`Robot 1 needed ${total1 / 100} steps per task`)
+    console.log(`Robot 2 needed ${total2 / 100}`)
+  }
+  
+  compareRobots(routeRobot, [], goalOrientedRobot, []);
+
+
+
+
+
+///////// THE 2ND TASK
+/*
+function lazyRobot({place, parcels}, route) {
+    if (route.length == 0) {
+      // Describe a route for every parcel
+      let routes = parcels.map(parcel => {
+        if (parcel.place != place) {
+          return {route: findRoute(roadGraph, place, parcel.place),
+                  pickUp: true};
+        } else {
+          return {route: findRoute(roadGraph, place, parcel.address),
+                  pickUp: false};
+        }
+      });
+  
+      // This determines the precedence a route gets when choosing.
+      // Route length counts negatively, routes that pick up a package
+      // get a small bonus.
+      function score({route, pickUp}) {
+        return (pickUp ? 0.5 : 0) - route.length;
+      }
+      route = routes.reduce((a, b) => score(a) > score(b) ? a : b).route;
+    }
+  
+    return {direction: route[0], memory: route.slice(1)};
+  }
+  
+  runRobotAnimation(VillageState.random(), lazyRobot, []);
+*/
+
+
+
+
+///////// THE 3RD TASK
+/*
+class PGroup {
+    constructor(members) {
+      this.members = members;
+    }
+  
+    add(value) {
+      if (this.has(value)) return this;
+      return new PGroup(this.members.concat([value]));
+    }
+  
+    delete(value) {
+      if (!this.has(value)) return this;
+      return new PGroup(this.members.filter(m => m !== value));
+    }
+  
+    has(value) {
+      return this.members.includes(value);
+    }
+  }
+  
+  PGroup.empty = new PGroup([]);
+  
+  let a = PGroup.empty.add("a");
+  let ab = a.add("b");
+  let b = ab.delete("a");
+  
+  console.log(b.has("b"));
+  // → true
+  console.log(a.has("b"));
+  // → false
+  console.log(b.has("a"));
+  // → false
+  */
